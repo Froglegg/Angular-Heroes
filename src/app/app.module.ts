fd3916f882838d4dd85c9e3a909eaa490c050778
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-02-19 16:49:15 -0500

    added pub/sub services, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { MessagesComponent } from "./components/messages/messages.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
