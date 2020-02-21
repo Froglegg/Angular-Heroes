ca9c86b7290ac642d20ad9d9f263a55576f679a9
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-02-21 15:56:37 -0500

    integrated http queries to server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
 import { MessagesComponent } from "./components/messages/messages.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./services/in-memory-data.service";
+import { HeroSearchComponent } from "./components/hero-search/hero-search.component";
 
 @NgModule({
   declarations: [
@@ -15,9 +18,22 @@ import { DashboardComponent } from "./components/dashboard/dashboard.component";
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
